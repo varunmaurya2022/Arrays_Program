10b410755eacc07efc159af5645b095c61142a77
varun maurya <varunmaurya@2022>
2023-11-02 22:45:36 +0530

    add two

--- Find_Max2.java
+++ Find_Max2.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
-public class Find_Max2{
-	public static void main(String... args){
-		Scanner sc=new Scanner(System.in);
+
+public class Find_Max2 {
+	public static void main(String... args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter The Array ");
-		int rows=sc.nextInt();
+		int rows = sc.nextInt();
 		System.out.println("Enter The Size ");
-		int size=sc.nextInt();
-		
-		int marks[][]=new int [rows][size];
+		int size = sc.nextInt();
+
+		int marks[][] = new int[rows][size];
 		System.out.println("Enter The element in array ");
-		for (int i=0; i<rows;i++){
-			for (int j=0; j<size;j++){
-				marks[i][j]=sc.nextInt();
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < size; j++) {
+				marks[i][j] = sc.nextInt();
 			}
 		}
+		sc.close();
 		System.out.println("The Array is ");
-		for (int i=0; i<rows;i++){
-			for (int j=0; j<size;j++){
-				System.out.print(marks[i][j]+" ");
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.print(marks[i][j] + " ");
 			}
 			System.out.println(" ");
 		}
 		System.out.println("");
-		int N=marks.length;
-		int M=marks[0].length;
-		System.out.print("The biggest value is  "+Show_max(marks,N,M));
+		int N = marks.length;
+		int M = marks[0].length;
+		System.out.print("The biggest value is  " + Show_max(marks, N, M));
 	}
-	public static int Show_max(int arr[][], int N, int M){
-		int max_value=arr[0][0];
-		for (int i=0;i<N; i++){
-			for (int j=0; j<M;j++){
-				if (arr[i][j]>max_value){
-					max_value=arr[i][j];
+
+	public static int Show_max(int arr[][], int N, int M) {
+		int max_value = arr[0][0];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] > max_value) {
+					max_value = arr[i][j];
 				}
 			}
 		}
 		return max_value;
 	}
 }
-	
